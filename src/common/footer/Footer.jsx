361799ad641b03f997baa8ee869276abb70223d2
MeshaalRaza <mishalraza983@gmail.com>
2025-01-19 20:04:26 +0500

    Add files via upload

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>Al-Fatah</h1>
+            <p>Al-Fatah is a mega store with over 50 stores in all over Pakistan, focused on providing the best services to people and get all utilities just under one roof.</p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>185 Building Tower Block Karachi, Pakistan</li>
+              <li>Email: Al-Fatah.help@gmail.com</li>
+              <li>Phone: +1 1100 960 890</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/MainPage/Sdata.js
@@ -0,0 +1,27 @@
+const Sdata = [
+  {
+    id: 1,
+    title: "50% Off For Your First Shopping",
+    desc: "Flat 50% off would be provided to our members and 100 lucky customers first. Go grab it now!",
+    cover: "./images/SlideCard/slide-1.png",
+  },
+  {
+    id: 2,
+    title: "Leading Store",
+    desc: "We are Pakistan's first mega store who have launched more tahn 50 stores in just a span of 1 year",
+    cover: "./images/SlideCard/slide-2.png",
+  },
+  {
+    id: 3,
+    title: "Quality over Large-Scale Production.",
+    desc:"We have testimonial from our customers all over the world, who are loyal to us because of the promotion deals, and effective services of quality we are offering",
+    cover: "./images/SlideCard/slide-3.png",
+  },
+  {
+    id: 4,
+    title: "Our customers' happiness is our Goal",
+    desc: "We go till the end measures to ensure that the best quality and price ranges to our customers.",
+    cover: "./images/SlideCard/slide-4.png",
+  },
+]
+export default Sdata
